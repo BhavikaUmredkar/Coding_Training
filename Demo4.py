7c698517afec1abb6f1fb45355ffabe0a5c2fa01
Bhavika Umredkar <bhavikaumredkar01@gmail.com>
2026-03-16 10:35:39 +0530

    update code

--- Demo4.py
+++ Demo4.py
@@ -33,7 +33,25 @@
 
 # 1. Positional argument
 
+# def personalInfo(fname, lname):
+#     print("First Nmae=", fname)
+#     print("Lat Name:", lname)
+# personalInfo("Bhavika", "Umredkar")
+
+
+# 2. Keyword argument
+
 def personalInfo(fname, lname):
     print("First Nmae=", fname)
-    print("Lat Nmae:", lname)
-personalInfo("Bhavika", "Umredkar")
+    print("Lat Name:", lname)
+fname = "Bhavika"
+lname = "Umredkar"
+personalInfo(fname, lname)
+
+
+# 3. Default argument
+def cityName(city):
+    print(city)
+cityName("Nagpur")
+cityName("Mumbai")
+cityName("Pune")
