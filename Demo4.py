edf3b7e98becf1e6211a12c5f4b2580d57c86ebd
Bhavika Umredkar <bhavikaumredkar01@gmail.com>
2026-03-16 10:29:42 +0530

    Added new file

---
+++ Demo4.py
@@ -0,0 +1,39 @@
+# Function
+# def msg(): # called function
+#     print("Hello World")  
+# msg()
+
+
+# def msg(): # called function
+#     n1 = int(input("Enter the value of n1: "))
+#     n2 = int(input("Enter the value of n2: "))
+#     print("Add =", n1+n2)
+# msg()
+
+
+# how to return multiple value
+# def opr():
+#     n1 = int(input("Enter the value of n1: "))
+#     n2 = int(input("Enter the value of n2: "))
+#     sum = n1+n2
+#     mul = n1*n2
+#     sub = n1-n2
+#     div = n1/n2
+#     return sum,sub,mul,div #return in tuple. It is immutable
+# result = opr()
+# print(result)
+
+
+# Four types of argument in python
+# 1. Positional argument
+# 2. Keyword argument
+# 3. Default argument
+# 4. Variable length argument/ variable number of argument 
+
+
+# 1. Positional argument
+
+def personalInfo(fname, lname):
+    print("First Nmae=", fname)
+    print("Lat Nmae:", lname)
+personalInfo("Bhavika", "Umredkar")
